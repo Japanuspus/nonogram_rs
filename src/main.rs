8add0d61b8e26be5154b458464f1ac81c4d91320
Janus <janus@insignificancegalore.net>
2020-08-13 06:58:54 +0200

    Simplify config factory

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::iter;
 
+#[derive(Debug)]
 struct ColumnConfigs<'a> {
     spec: &'a Vec<usize>,
     spaces: Vec<usize>,
@@ -13,26 +14,16 @@ impl Iterator for ColumnConfigs<'_> {
     }
 }
 
-fn column_configs_as_option<'a>(spec: &'a Vec<usize>, size: usize) -> Option<ColumnConfigs<'a>> {
-    let spaces: Vec<_> = iter::repeat(0).take(spec.len()).collect();
-    let maybe_n_space = (size+1).checked_sub(spec.len()+spec.iter().sum::<usize>());
-    if let Some(n_space) = maybe_n_space {
-        Some(ColumnConfigs{spec, spaces, n_space})
-    } else { None }
-}
-
-/// Return iterator over all possible configurations of blocks of length
-/// as specified in spec. Each configuration is given as a `Vec<bool>` of length
-/// size.
-/// If requested size cannot be satisfied, an empty iterator is returned.
-fn column_configs_as_empty<'a>(spec: &'a Vec<usize>, size: usize) -> 
-impl Iterator<Item = Vec<bool>> + 'a {
-    column_configs_as_option(spec, size)
-    .into_iter().flat_map(move |some_iter| some_iter)
+fn column_configs<'a>(spec: &'a Vec<usize>, size: usize) -> ColumnConfigs<'a> {
+    ColumnConfigs{
+        spec, 
+        spaces: vec![0; spec.len()], 
+        n_space: (size+1).checked_sub(spec.len()+spec.iter().sum::<usize>()).expect("Provided size below min requirement")
+    }
 }
 
 fn main() {
-    for cfg in column_configs_as_empty(&vec![2,2], 6) {
+    for cfg in column_configs(&vec![2,2], 6) {
         println!("Possible config: {:?}", cfg);
     }
 }
